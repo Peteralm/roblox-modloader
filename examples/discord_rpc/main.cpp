a6e191f553d13fbddde84e3d57d5d61d7fab5c18
Revolution <tavinho.daniel@gmail.com>
2026-09-13 20:05:51 -0300

    refactor(discord_rpc): replace DISCORD_RPC_MOD_API with RML_MOD_ABI_EXPORT for mod functions

--- examples/discord_rpc/main.cpp
+++ examples/discord_rpc/main.cpp
@@ -174,14 +174,12 @@ private:
 };
 
 
-#define DISCORD_RPC_MOD_API __declspec(dllexport)
-
 extern "C" {
-DISCORD_RPC_MOD_API ModBase *start_mod() {
+RML_MOD_ABI_EXPORT ModBase *start_mod() {
     return new discord_rpc();
 }
 
-DISCORD_RPC_MOD_API void uninstall_mod(const ModBase *mod) {
+RML_MOD_ABI_EXPORT void uninstall_mod(const ModBase *mod) {
     delete mod;
 }
 }
